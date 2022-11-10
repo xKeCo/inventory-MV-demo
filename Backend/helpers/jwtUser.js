74c75108004834d20ca5efb47e9d5481cdde3b3c
Kevin Collazos <kevcollazos@gmail.com>
2022-11-09 19:52:49 -0500

    Eliminacion de la carpeta backend

--- Backend/helpers/jwtUser.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateJWT = id => {
-    return jwt.sign({ id }, process.env.JWT_PASS, {
-        expiresIn: '6h',
-    })
-};
-
-module.exports = generateJWT;
